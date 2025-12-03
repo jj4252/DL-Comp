637fbe813e0f17f8e4988f7549c6c44fa7328519
dongYoun2 <dlehddbs6425@gmail.com>
2025-12-03 18:13:57 -0500

    open images

--- README.md
+++ README.md
@@ -9,13 +9,18 @@ curl -L -O https://storage.googleapis.com/openimages/v7/oidv7-class-descriptions
 curl -L -O https://storage.googleapis.com/openimages/v7/oidv7-train-annotations-human-imagelabels.csv
 curl -L -O https://storage.googleapis.com/openimages/v6/oidv6-train-images-with-labels-with-rotation.csv
 
-# Run all cells in the notebook/open_images_analysis.ipynb from the PROJECT ROOT
+# From the PROJECT ROOT:
+# Run all cells in the notebook/mini_imagenet_class.ipynb
+# Run all cells in the notebook/open_images_analysis2.ipynb
 
-# cd to the POJECT ROOT and run the following commands
-# sample size should be a multiple of 1000
-python scripts/sample_open_images.py --sample-size=200000
 
-python scripts/download_open_images.py data/Open_Images/train_sampled_200k.txt  --download_folder=data/Open_Images/train/raw --num_processes=5
+python scripts/sample_open_images.py --start 1 --end 150000
+
+python scripts/download_open_images.py data/Open_Images/mini_only_1-150000.txt  --download_folder=data/Open_Images/train/raw/mini_only_1-150000 --num_processes=5
+
+python scripts/download_open_images.py data/Open_Images/non_only_1-150000.txt  --download_folder=data/Open_Images/train/raw/non_only_1-150000 --num_processes=5
+
+python scripts/download_open_images.py data/Open_Images/mixed_1-150000.txt  --download_folder=data/Open_Images/train/raw/mixed_1-150000 --num_processes=5
 
 # or on HPC Cluster
 sbatch sbatch/download_open_images.slurm
--- scripts/sample_open_images.py
+++ scripts/sample_open_images.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 from pathlib import Path
 
@@ -7,33 +6,77 @@ import pandas as pd
 
 def main():
     parser = argparse.ArgumentParser(description="Sample Open Images dataset")
-    parser.add_argument("--sample-size", type=int, default=200_000, help="Number of images to sample (default: 200000)")
+
+    parser.add_argument("--start", type=int, required=True, help="Start index of the sample")
+    parser.add_argument("--end", type=int, required=True, help="End index of the sample")
+
 
     args = parser.parse_args()
-    sample_size = args.sample_size
-    assert sample_size % 1000 == 0, "Sample size must be a multiple of 1000"
 
-    data_dir = Path("data/Open_Images")
-    data_dir.mkdir(parents=True, exist_ok=True)
-    train_parquet_file = data_dir / "train_list.parquet"
+    if args.start < 1:
+        parser.error("--start must be non-negative")
 
-    print("Reading train parquet file...")
-    train_df = pd.read_parquet(train_parquet_file)
-    print(f"Loaded {len(train_df):,} images")
+    if args.end <= args.start:
+        parser.error("--end must be greater than --start")
 
-    print(f"Sampling {sample_size:,} images...")
-    sample_df = train_df.sample(n=sample_size, random_state=42)
-    sample_df.drop(columns=["Labels"], inplace=True)
+    OI_data_dir = Path("data/Open_Images")
+    mini_only_parquet_file = OI_data_dir / "mini_only_train_list.parquet"
+    non_only_parquet_file = OI_data_dir / "non_only_train_list.parquet"
+    mixed_parquet_file = OI_data_dir / "mixed_train_list.parquet"
 
-    sample_csv_file = data_dir / f"train_sampled_{sample_size//1000}k.csv"
-    print(f"Saving to {sample_csv_file}...")
-    sample_df.to_csv(sample_csv_file, index=False)
+    print("Reading mini_only, non_only, and mixed train parquet files...")
+    mini_only_train_df = pd.read_parquet(mini_only_parquet_file)
+    non_only_train_df = pd.read_parquet(non_only_parquet_file)
+    mixed_train_df = pd.read_parquet(mixed_parquet_file)
 
-    # need this file for downloading the images
-    image_list_file = data_dir / f"train_sampled_{sample_size//1000}k.txt"
-    print(f"Writing image list to {image_list_file}...")
-    with open(image_list_file, "w") as f:
-        for image_id in sample_df["ImageID"]:
+    print(f"len(mini_only_train_df): {len(mini_only_train_df)}")
+    print(f"len(non_only_train_df): {len(non_only_train_df)}")
+    print(f"len(mixed_train_df): {len(mixed_train_df)}")
+
+    assert args.end <= len(mini_only_train_df), f"End index ({args.end}) exceeds mini_only dataset size ({len(mini_only_train_df)})"
+    assert args.end <= len(non_only_train_df), f"End index ({args.end}) exceeds non_only dataset size ({len(non_only_train_df)})"
+    assert args.end <= len(mixed_train_df), f"End index ({args.end}) exceeds mixed dataset size ({len(mixed_train_df)})"
+
+
+    print(f"Selecting from {args.start}th to {args.end}th rows for each dataframe...")
+
+    s_mini_only_df = mini_only_train_df.iloc[args.start-1:args.end].copy()
+    s_non_only_df = non_only_train_df.iloc[args.start-1:args.end].copy()
+    s_mixed_df = mixed_train_df.iloc[args.start-1:args.end].copy()
+
+    s_mini_only_df.drop(columns=["Labels"], inplace=True)
+    s_non_only_df.drop(columns=["Labels"], inplace=True)
+    s_mixed_df.drop(columns=["Labels"], inplace=True)
+
+    file_suffix = f"{args.start}-{args.end}"
+
+    s_mini_only_csv_file = OI_data_dir / f"mini_only_{file_suffix}.csv"
+    s_non_only_csv_file = OI_data_dir / f"non_only_{file_suffix}.csv"
+    s_mixed_csv_file = OI_data_dir / f"mixed_{file_suffix}.csv"
+
+    print(f"Saving to {s_mini_only_csv_file}...")
+    s_mini_only_df.to_csv(s_mini_only_csv_file, index=False)
+    print(f"Saving to {s_non_only_csv_file}...")
+    s_non_only_df.to_csv(s_non_only_csv_file, index=False)
+    print(f"Saving to {s_mixed_csv_file}...")
+    s_mixed_df.to_csv(s_mixed_csv_file, index=False)
+
+    # need these files for downloading actual images using scripts/download_open_images.py
+    mini_only_image_list_file = OI_data_dir / f"mini_only_{file_suffix}.txt"
+    non_only_image_list_file = OI_data_dir / f"non_only_{file_suffix}.txt"
+    mixed_image_list_file = OI_data_dir / f"mixed_{file_suffix}.txt"
+
+    print(f"Writing image list to {mini_only_image_list_file}...")
+    with open(mini_only_image_list_file, "w") as f:
+        for image_id in s_mini_only_df["ImageID"]:
+            f.write(f"train/{image_id}\n")
+    print(f"Writing image list to {non_only_image_list_file}...")
+    with open(non_only_image_list_file, "w") as f:
+        for image_id in s_non_only_df["ImageID"]:
+            f.write(f"train/{image_id}\n")
+    print(f"Writing image list to {mixed_image_list_file}...")
+    with open(mixed_image_list_file, "w") as f:
+        for image_id in s_mixed_df["ImageID"]:
             f.write(f"train/{image_id}\n")
 
     print("Done!")
